086ef8f156c09f90fdb530fb6ced62fc91d65a59
岳大禹 <yuedayu13@gmail.com>
2015-06-15 22:36:05 +0800

    神经网络：训练基本神经网络，能跑了。。

--- training/NeuralNetwork/NN_training.py
+++ training/NeuralNetwork/NN_training.py
@@ -1,11 +1,47 @@
 __author__ = 'Yue Dayu'
 
 from pybrain.tools.shortcuts import buildNetwork
-from pybrain.datasets import ClassificationDataSet
+from pybrain.datasets import SupervisedDataSet
 from pybrain.tools.customxml import NetworkWriter
 from pybrain.supervised.trainers import BackpropTrainer
 import numpy as np
 import sys
+
+# from pybrain.datasets import SupervisedDataSet
+#
+# dataModel = [
+#     [(0,0), (0,0)],
+#     [(0,1), (0,1)],
+#     [(1,0), (1,0)],
+#     [(1,1), (1,1)],
+# ]
+#
+# ds = SupervisedDataSet(2, 2)
+# for input, target in dataModel:
+#     ds.addSample(input, target)
+#
+# # create a large random data set
+# import random
+# random.seed()
+# trainingSet = SupervisedDataSet(2, 2);
+# for ri in range(0,1000):
+#     input,target = dataModel[random.getrandbits(2)];
+#     trainingSet.addSample(input, target)
+#
+# from pybrain.tools.shortcuts import buildNetwork
+# net = buildNetwork(2, 2, 2, bias=True)
+#
+# from pybrain.supervised.trainers import BackpropTrainer
+# trainer = BackpropTrainer(net, ds, learningrate = 0.001, momentum = 0.99)
+# trainer.trainUntilConvergence(verbose=True,
+#                               trainingData=trainingSet,
+#                               validationData=ds,
+#                               maxEpochs=10)
+#
+# print '0,0->', net.activate([0,0])
+# print '0,1->', net.activate([0,1])
+# print '1,0->', net.activate([1,0])
+# print '1,1->', net.activate([1,1])
 
 labals = ['n03877845', 'n04515003', 'n01923025', 'n03767203', 'n11669921', 'n07897438', 'n10247358', 'n01613177', 'n04583620', 'n02278980']
 
@@ -23,18 +59,42 @@ def readfile(filename):
     result = np.array(feature_list)
     return type_list, result
 
-def neural_train(filename, output):
+def trans(name):
+    res = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    res[labals.index(name)] = 1
+    return res
+
+def max_index(list):
+    max = list[0]
+    index = 0
+    i = 0
+    for x in list:
+        if x > max:
+            max = x
+            index = i
+        i += 1
+    return index
+
+def neural_train(filename, testfile, output):
     tag, data = readfile(filename)
-    net = buildNetwork(len(data[0]), 3, 1)
-    ds = ClassificationDataSet(len(data[0]), 10, labals)
-    for x in range(0, len(data[0])):
-        ds.addSample(data[x], [labals.index(tag[x])])
-    trainer = BackpropTrainer(net)
-    trainer.setData(ds)
+    testtag, testdata = readfile(testfile)
+    net = buildNetwork(len(data[0]), 80, 20, 10)
+    ds = SupervisedDataSet(len(data[0]), 10)
+    for x in range(0, len(data)):
+        ds.addSample(data[x], trans(tag[x]))
+    testds = SupervisedDataSet(len(data[0]), 10)
+    for x in range(0, len(testdata)):
+        testds.addSample(testdata[x], trans(testtag[x]))
+    trainer = BackpropTrainer(net, ds)
     print "training..."
-    trainer.train()
+    trainer.trainUntilConvergence(verbose=True,
+                              trainingData=ds,
+                              validationData=testds,
+                              maxEpochs=30)
     print "done"
-    NetworkWriter.writeToFile(net, 'testNet.xml')
+    NetworkWriter.writeToFile(net, output)
+
 
 if __name__ == '__main__':
-    neural_train(sys.argv[1], sys.argv[2])
+    neural_train(sys.argv[1], sys.argv[2], sys.argv[3])
+
